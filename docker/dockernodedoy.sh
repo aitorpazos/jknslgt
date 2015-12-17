0423d49bff513fd9a82d5691ee8e0f40aa30f64b
Aitor Pazos <mail@aitorpazos.es>
2015-12-17 01:15:47 +0000

    Do exit the script on termination signal.

--- docker/dockernodedoy.sh
+++ docker/dockernodedoy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CONFIG_DIR=<Replace with the DIR you want to put your config in>
-trap "docker stop nodedoy" SIGINT SIGTERM
+trap "docker stop nodedoy; exit 0" SIGINT SIGTERM
 
 if [ "$(docker ps -a --format '{{.Names}}' | grep nodedoy)" == "" ]; then
-docker run -p 8085:8085 --name nodedoy -v ${CONFIG_DIR}:/config --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --device /dev/snd:/dev/snd --privileged aitorpazos/nodedoy:latest;
+	docker run -p 8085:8085 --name nodedoy -v ${CONFIG_DIR}:/config --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --device /dev/snd:/dev/snd --privileged aitorpazos/nodedoy:latest;
 else
 	docker start nodedoy
 fi
